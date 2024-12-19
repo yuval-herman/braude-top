93323142e346f9d12fd93e1ea5f9dff1d2d89624
Yuval Herman <yuvalherman99@gmail.com>
2024-12-19 16:48:47 +0200

    removed comment from TimeTable story

--- src/stories/Timetable.stories.svelte
+++ src/stories/Timetable.stories.svelte
@@ -2,7 +2,6 @@
 	import { defineMeta } from '@storybook/addon-svelte-csf';
 	import TimeTable from '$lib/components/TimeTable.svelte';
 
-	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 	const { Story } = defineMeta({
 		title: 'TimeTable',
 		component: TimeTable
