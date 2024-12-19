fa872d8468c1104a911d966fc0810e364809910f
Yuval Herman <yuvalherman99@gmail.com>
2024-12-19 16:43:11 +0200

    changed type to interface and added some docs

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,6 @@
-export type Item = { day: number; start: number; end: number; value: string };
+export interface Item {
+	day: number; // Day of the week 1-6
+	start: number; // Start hour as an integer (1=8:30, 2=9:30, etc...)
+	end: number; // End hour as an integer
+	value: string; // Value to show in cell
+}
