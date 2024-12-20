2d023eb03b770bfcb2cb6342361a62f26e1f6f90
Yuval Herman <yuvalherman99@gmail.com>
2024-12-20 15:04:09 +0200

    added PaginatedList component

---
+++ src/lib/components/PaginatedList.svelte
@@ -0,0 +1,33 @@
+<!-- success!
+1. 1 string item
+2. multiple items
+3. items can be deleted and added
+4. pages
+-->
+<script lang="ts">
+	type ListItem = string;
+	const { items }: { items: ListItem[] } = $props();
+</script>
+
+<ul>
+	{#each items as item}
+		<li>{item}</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		background-color: var(--bg);
+		padding: 8px;
+	}
+	li {
+		background-color: var(--top-bg);
+		margin: 12px;
+		padding: 8px;
+		padding-right: 16px;
+		border-radius: 12px;
+	}
+</style>
---
+++ src/stories/PaginatedList.stories.svelte
@@ -0,0 +1,15 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import PaginatedList from '$lib/components/PaginatedList.svelte';
+
+	const { Story } = defineMeta({
+		title: 'PaginatedList',
+		component: PaginatedList
+	});
+	const items = ['מתמטיקה', 'פיזיקה', 'כימיה', 'ספרות', 'היסטוריה', 'אנגלית'];
+</script>
+
+<Story name="Empty" />
+<Story name="One page" args={{ items }} />
+<Story name="Three pages" args={{ items: Array(6).fill(items).flat() }} />
+<Story name="Many pages" args={{ items: Array(100).fill(items).flat() }} />
