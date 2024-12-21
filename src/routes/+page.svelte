a8d818b1648713a776bfbd3b36475f4b6fc30724
Yuval Herman <yuvalherman99@gmail.com>
2024-12-21 18:20:15 +0200

    added PaginatedList to main route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import TimeTable from '$lib/components/TimeTable.svelte';
+	import PaginatedList from '$lib/components/PaginatedList.svelte';
 </script>
 
-<TimeTable items={[{ value: 'test', day: 3, end: 6, start: 2 }]} />
+<main>
+	<PaginatedList items={[]} />
+	<TimeTable items={[{ value: 'test', day: 3, end: 6, start: 2 }]} />
+</main>
+
+<style>
+	main {
+		height: 100%;
+		display: grid;
+		grid-template-columns: 1fr 2fr;
+	}
+</style>
