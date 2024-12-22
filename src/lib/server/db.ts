2bb7133740ac24eceeaedcb0d25627b36d004d48
Yuval Herman <yuvalherman99@gmail.com>
2024-12-22 11:14:37 +0200

    added db function

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,6 +1,26 @@
+import type { CourseInstance, Session } from '$lib/types';
 import Database from 'better-sqlite3';
 
 const db = new Database('src/lib/courses.db');
 
-const p = db.prepare<[], string>('SELECT name FROM courses ORDER by name').pluck();
-export const getCourseNames = () => p.all();
+/** Retrieves all the courses names and ids */
+export const getCoursesMinimal = (() => {
+	const stmt = db.prepare<[], { course_id: number; name: string }>(
+		'SELECT course_id, name FROM courses ORDER by name'
+	);
+	return () => stmt.all();
+})();
+
+/** Retrieves all the course instances for a given course id */
+export const getCourseInstances = (() => {
+	const stmt = db.prepare<number, CourseInstance>(
+		'SELECT * FROM course_instances WHERE course_id = ?'
+	);
+	return (id: number | string) => stmt.all(id as number);
+})();
+
+/** Retrieves all the instance session for a given course instance id */
+export const getInstancesSession = (() => {
+	const stmt = db.prepare<number, Session>('SELECT * from sessions where course_instance_id = ?');
+	return (id: number | string) => stmt.all(id as number);
+})();
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,24 @@ export interface Item {
 	end: number; // End hour as an integer
 	value: string; // Value to show in cell
 }
+
+export interface CourseInstance {
+	course_id: number;
+	course_instance_id: number;
+	type: string;
+	hours: number;
+	group_name: string;
+	is_full: number;
+	language: string;
+	extra_notes?: string;
+}
+
+export interface Session {
+	course_instance_id: number;
+	semester: string;
+	week_day: string;
+	start_time: string;
+	end_time: string;
+	instructor: string;
+	room: string;
+}
