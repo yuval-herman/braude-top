bec4764776309744ff851286a973e6a2a8327dcd
Yuval Herman <yuvalherman99@gmail.com>
2024-12-24 14:39:04 +0200

    added db functions to retrieve only courses with sessions

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,9 +3,19 @@ import Database from 'better-sqlite3';
 
 const db = new Database('src/lib/courses.db');
 
-/** Retrieves all the courses names and ids */
+/** Retrieves all the courses */
 export const getCourses = (() => {
 	const stmt = db.prepare<[], Course>('SELECT * FROM courses ORDER by name');
+	return () => stmt.all();
+})();
+
+/** Retrieves all the courses that have sessions*/
+export const getNonEmptyCourses = (() => {
+	const stmt = db.prepare<[], Course>(
+		'SELECT distinct course_id, name, year, credit, description, syllabus_link from courses \
+		join course_instances using (course_id) \
+		join sessions USING (course_instance_id)'
+	);
 	return () => stmt.all();
 })();
 
@@ -13,6 +23,16 @@ export const getCourses = (() => {
 export const getCourseInstances = (() => {
 	const stmt = db.prepare<number, CourseInstance>(
 		'SELECT * FROM course_instances WHERE course_id = ?'
+	);
+	return (id: number | string) => stmt.all(id as number);
+})();
+
+/** Retrieves all the course instances for a given course id that have sessions*/
+export const getNonEmptyCourseInstances = (() => {
+	const stmt = db.prepare<number, CourseInstance>(
+		'SELECT course_id, course_instance_id, instructor, type, hours, group_name, is_full, language, extra_notes from course_instances \
+		JOIN sessions USING (course_instance_id) \
+		WHERE course_id = ?'
 	);
 	return (id: number | string) => stmt.all(id as number);
 })();
