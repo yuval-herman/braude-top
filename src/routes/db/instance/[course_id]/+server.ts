c3cc88ecb44118ff9b9dfc78ef08c8efbf181913
Yuval Herman <yuvalherman99@gmail.com>
2025-01-01 15:01:19 +0200

    removed unused api routes

--- src/routes/db/instance/[course_id]/+server.ts
+++
@@ -1,8 +0,0 @@
-import { error, json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import { getCourseInstances } from '$lib/server/db';
-
-export const GET: RequestHandler = async ({ params }) => {
-	const { course_id } = params;
-	return json(getCourseInstances(course_id));
-};
--- src/routes/db/sessions/[instance_id]/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import { getInstancesSession } from '$lib/server/db';
-
-export const GET: RequestHandler = async ({ params }) => {
-	const { instance_id } = params;
-	return json(getInstancesSession(instance_id));
-};
