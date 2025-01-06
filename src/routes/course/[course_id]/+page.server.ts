b9ba5af4782eb189e9507f2dab7a1c2f24da2133
Yuval Herman <yuvalherman99@gmail.com>
2025-01-06 14:34:19 +0200

    removed unused type import

--- src/routes/course/[course_id]/+page.server.ts
+++ src/routes/course/[course_id]/+page.server.ts
@@ -1,7 +1,6 @@
 import { getFullCourse } from '$lib/server/db';
-import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params, setHeaders }) => {
+export const load = async ({ params, setHeaders }) => {
 	setHeaders({ 'cache-control': 'max-age=3600' });
 	return { course: getFullCourse(params.course_id) };
 };
