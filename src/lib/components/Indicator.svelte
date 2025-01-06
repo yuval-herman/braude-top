39441d904d5f0545b863618cda0a657be7455a67
Yuval Herman <yuvalherman99@gmail.com>
2025-01-06 18:32:01 +0200

    added s border to indicator

--- src/lib/components/Indicator.svelte
+++ src/lib/components/Indicator.svelte
@@ -15,6 +15,7 @@
 		bottom: 0;
 		border-top-right-radius: inherit;
 		border-bottom-right-radius: inherit;
+		border-left: 1px solid var(--light);
 		background: var(--indicator-color);
 		width: 8px;
 	}
