da56c1714de104da7ddcb0d24893d29a47e77580
Yuval Herman <yuvalherman99@gmail.com>
2025-01-08 15:12:17 +0200

    added expected elements test in mobile

--- e2e/mainPage.mobile.test.ts
+++ e2e/mainPage.mobile.test.ts
@@ -1,12 +1,48 @@
 import test, { expect } from '@playwright/test';
 
+const hoursList = [
+	'8:30',
+	'9:30',
+	'10:30',
+	'11:30',
+	'12:20',
+	'הפסקת צהריים',
+	'12:50',
+	'13:50',
+	'14:50',
+	'15:50',
+	'16:50',
+	'17:50',
+	'18:50',
+	'19:50',
+	'20:50',
+	'21:50',
+	'22:50',
+];
+
 test.describe('main page', () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/');
 		page.locator('#driver-popover-content').press('Escape');
 	});
 
-	test('make sure course list is scrollable in mobile', async ({ page }) => {
+	test('home page has expected elements', async ({ page }) => {
+		await expect(page.locator('nav').filter({ hasText: 'ראשי' })).toBeVisible();
+		const table = page.locator('table');
+		await expect(table).toBeVisible();
+		const tableHead = table.locator('thead');
+		await expect(tableHead.locator('th')).toHaveCount(7);
+		const tableBody = table.locator('tbody');
+		const rows = tableBody.locator('tr');
+		await expect(rows).toHaveCount(hoursList.length);
+		for (let i = 0; i < hoursList.length; i++) {
+			await expect(rows.nth(i).locator('th').first()).toContainText(hoursList[i]);
+			await expect(rows.nth(i).locator('td')).toHaveCount(6);
+		}
+		await expect(page.locator('header ~ ul')).toBeHidden();
+	});
+
+	test('make sure course list is scrollable', async ({ page }) => {
 		await page.getByRole('button', { name: 'רשימת קורסים' }).click();
 		const courseList = page.locator('header ~ ul');
 		const isScrollable = await courseList.evaluate((el) => el.scrollHeight > el.clientHeight);
