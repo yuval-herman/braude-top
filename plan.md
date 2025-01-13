84039b32ac10b89e350d04f5a54fea617f9270d3
Yuval Herman <yuvalherman99@gmail.com>
2025-01-13 22:41:18 +0200

    added beta page

--- plan.md
+++ plan.md
@@ -2,13 +2,11 @@
 
 # Planned
 
-- rewrite tests, make them deterministic using mock db
 - add site info panel (visits, last course update etc...)
 
-- add beta.braude.top domain to test site on the web (maybe allow users to check beta features?)
 - make sure user is aware if he is selecting multiple (unrequired) sessions (such as too many lectures)
 
-- save schedule for every semester on its own
+- rewrite tests, make them deterministic using mock db
 
 - add ctrl-y to redo
 - add users comments on course page (ratings, comments)
--- utilities/deploy.js
+++ utilities/deploy.js
@@ -1,6 +1,10 @@
 // @ts-check
 import { execSync } from 'child_process';
+import { argv } from 'process';
 import secrets from '../secrets.json' with { type: 'json' };
+
+const prod = argv[2] === 'prod';
+const servicename = prod ? 'braude-scheduler' : 'braude-beta-scheduler';
 
 /**
  * @param {string[]} commands
@@ -16,16 +20,17 @@ const isoTime = new Date().toISOString();
 // Build app
 execSync('npm run build', { stdio: 'inherit' });
 
+const backupFolder = `${prod ? 'prod' : 'beta'}-${isoTime}`;
 runCommands([
 	// Stop, backup db, and remove app from server
-	'systemctl stop braude-scheduler.service',
-	`mkdir ${isoTime}`,
-	`mv braude-scheduler/data ${isoTime}`,
-	'rm -rf braude-scheduler',
+	`systemctl stop ${servicename}.service`,
+	`mkdir ${backupFolder}`,
+	`mv ${servicename}/data ${backupFolder}`,
+	`rm -rf ${servicename}`,
 
 	// Recreate and copy backup into it
-	'mkdir -p braude-scheduler/data',
-	`cp -r ${isoTime}/data braude-scheduler`,
+	`mkdir -p ${servicename}/data`,
+	`cp -r ${backupFolder}/data ${servicename}`,
 ]);
 
 // Compress build
@@ -37,7 +42,7 @@ execSync(`scp -r build.tar.gz ${secrets.server}:/root/`, { stdio: 'inherit' });
 execSync('rm build.tar.gz');
 
 runCommands([
-	'cd braude-scheduler',
+	`cd ${servicename}`,
 
 	// Extract files and move build files out
 	'tar -zxf ../build.tar.gz',
@@ -51,5 +56,5 @@ runCommands([
 	'cp ../braude-fetcher/courses.db data/',
 
 	// Start the server
-	'systemctl start braude-scheduler.service',
+	`systemctl start ${servicename}.service`,
 ]);
