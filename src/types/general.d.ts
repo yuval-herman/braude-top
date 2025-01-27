4ad7407fc67b337b3ee54988ea3c1d620212cfa9
Yuval Herman <yuvalherman99@gmail.com>
2025-01-27 12:09:10 +0200

    fix type issue for Item

--- src/types/general.d.ts
+++ src/types/general.d.ts
@@ -14,7 +14,7 @@ interface EmptyRoomItemValue extends BaseItemValue<'empty-room'> {
 
 type ItemValueTypes = SessionItemValue | CustomItemValue | EmptyRoomItemValue;
 
-interface Item<TValue extends BaseItemValue = ItemValueTypes> {
+interface Item<TValue extends ItemValueTypes = BaseItemValue> {
 	day: number; // Day of the week 1-6
 	start: number; // Start hour as an integer (1=8:30, 2=9:30, etc...)
 	end: number; // End hour as an integer
