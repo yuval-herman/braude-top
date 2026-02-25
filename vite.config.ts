346090b17d37ad135632cee8ed340ac94816df5f
Yuval Herman <yuvalherman99@gmail.com>
2026-02-25 09:33:23 +0200

    disable storybook tests
    For now I disabled the storybook tests. They weren't used anyways and
    had conflicts with units tests.

--- vite.config.ts
+++ vite.config.ts
@@ -8,36 +8,34 @@ import { playwright } from '@vitest/browser-playwright';
 const dirname =
 	typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
-		projects: [
-			{
-				extends: true,
-				plugins: [
-					// The plugin will run tests for the stories defined in your Storybook config
-					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-					storybookTest({
-						configDir: path.join(dirname, '.storybook'),
-					}),
-				],
-				test: {
-					name: 'storybook',
-					browser: {
-						enabled: true,
-						headless: true,
-						provider: playwright({}),
-						instances: [
-							{
-								browser: 'chromium',
-							},
-						],
-					},
-					setupFiles: ['.storybook/vitest.setup.ts'],
-				},
-			},
-		],
+		// projects: [
+		// 	{
+		// 		plugins: [
+		// 			// The plugin will run tests for the stories defined in your Storybook config
+		// 			// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+		// 			storybookTest({
+		// 				configDir: path.join(dirname, '.storybook'),
+		// 			}),
+		// 		],
+		// 		test: {
+		// 			name: 'storybook',
+		// 			browser: {
+		// 				enabled: true,
+		// 				headless: true,
+		// 				provider: playwright({}),
+		// 				instances: [
+		// 					{
+		// 						browser: 'chromium',
+		// 					},
+		// 				],
+		// 			},
+		// 			setupFiles: ['.storybook/vitest.setup.ts'],
+		// 		},
+		// 	},
+		// ],
 	},
 });
