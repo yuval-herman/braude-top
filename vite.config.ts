21a8433f0ec1de08a696c96bf70ef24c36bddd84
Yuval Herman <yuvalherman99@gmail.com>
2026-07-07 14:37:06 +0300

    removed storybook

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
 const dirname =
 	typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
@@ -12,30 +11,5 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
-		// projects: [
-		// 	{
-		// 		plugins: [
-		// 			// The plugin will run tests for the stories defined in your Storybook config
-		// 			// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-		// 			storybookTest({
-		// 				configDir: path.join(dirname, '.storybook'),
-		// 			}),
-		// 		],
-		// 		test: {
-		// 			name: 'storybook',
-		// 			browser: {
-		// 				enabled: true,
-		// 				headless: true,
-		// 				provider: playwright({}),
-		// 				instances: [
-		// 					{
-		// 						browser: 'chromium',
-		// 					},
-		// 				],
-		// 			},
-		// 			setupFiles: ['.storybook/vitest.setup.ts'],
-		// 		},
-		// 	},
-		// ],
 	},
 });
