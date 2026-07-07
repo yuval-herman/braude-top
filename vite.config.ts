f97fe90771a51911aac11c8cf2e4cb9e5082a88b
Yuval Herman <yuvalherman99@gmail.com>
2026-07-07 15:10:20 +0300

    fixed lightdark css not working
    This wasn't a bug in my code, rather a bug in lightning css coming via
    vite. Hopefully I can remove this code at some point in the future when
    upstream gets fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { playwright } from '@vitest/browser-playwright';
-const dirname =
-	typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+import browserslist from 'browserslist';
+import { browserslistToTargets, Features } from 'lightningcss';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	css: {
+		lightningcss: {
+			targets: browserslistToTargets(browserslist('defaults')),
+			exclude: Features.LightDark,
+		},
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
